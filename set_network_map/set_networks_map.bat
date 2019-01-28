f74e30095c316c5e05aea5804992b12e94fc9e2b
matteo <matteo.lacki@gmail.com>
2019-01-28 17:57:28 +0100

    Silly little scripts for the lab of Stefan.

---
+++ set_network_map/set_networks_map.bat
@@ -0,0 +1,8 @@
+rem consisten mapping of drives
+net use I: \\msserver\idefix_rawdata
+net use O: \\msserver\obelix_rawdata
+net use Q: \\msserver\synapt
+net use J: \\msserver\idefix
+net use B: \\msserver\backup
+net use U: \\msserver\users
+net use L: \\msserver\legacy
---
+++ set_network_map/unset_networks_map.bat
@@ -0,0 +1,8 @@
+rem unset all the network drives
+net use I: /delete
+net use O: /delete
+net use Q: /delete
+net use J: /delete
+net use B: /delete
+net use U: /delete 
+net use L: /delete
