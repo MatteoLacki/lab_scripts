68c1ac3d9f4ee8c5592825fb6ca7e07f7dd6bbdb
matteo <matteo.lacki@gmail.com>
2019-01-30 13:46:55 +0100

    Added some verbosity.

--- stats_viewer/stats_summary.py
+++ stats_viewer/stats_summary.py
@@ -6,6 +6,7 @@ import pandas as pd
 from parsers import parsers, get_sampleSetNo_rawFileNo, get_parse
 
 
+verbose = True
 folder_path, csv_path = sys.argv[1:3]
 # folder_path = "/home/matteo/Projects/lab_scripts/data/RES"
 # csv_path = "test.csv"
@@ -28,10 +29,12 @@ def iter_outputs(folder_path, file_patterns):
 def get_rows(outputs, parse):
 	for root, files in outputs:
 		row = {}
-		row['sample_set_no'], row['raw_file_no'] = get_sampleSetNo_rawFileNo(root)
+		ss, rf = row['sample_set_no'], row['raw_file_no'] = get_sampleSetNo_rawFileNo(root)
 		for file in files:
 			row.update(parse(root, file))
 		yield row
+		if verbose:
+			print("Finished sample set {} and raw file {}.".format(ss, rf))
 
 parse = get_parse(parsers)
 outputs = iter_outputs(folder_path, parsers.keys())
