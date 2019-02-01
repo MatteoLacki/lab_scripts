9ada3232167cad6200751283690d76f7e38f7163
matteo <matteo.lacki@gmail.com>
2019-02-01 13:50:05 +0100

    Adding reset network drives.

--- set_network_map/set_network_maps.bat
+++ set_network_map/set_network_maps.bat
@@ -1,10 +1,10 @@
 rem consistent mapping of drives
 net use I: \\msserver\idefix_rawdata
 net use O: \\msserver\obelix_rawdata
-net use Q: \\msserver\synapt
-net use J: \\msserver\idefix
-net use P: \\msserver\backup
+net use S: \\msserver\synapt
+net use T: \\msserver\idefix
+net use Z: \\msserver\backup
 net use U: \\msserver\users
 net use L: \\msserver\legacy
 net use X: \\msserver\data
-net use S: \\msserver\symphony
+net use Y: \\msserver\symphony
--- set_network_map/unset_network_maps.bat
+++ set_network_map/unset_network_maps.bat
@@ -1,10 +1,10 @@
 rem unset all the network drives
 net use I: /delete
 net use O: /delete
-net use Q: /delete
-net use J: /delete
-net use P: /delete
-net use U: /delete 
-net use L: /delete
 net use S: /delete
+net use T: /delete
+net use Z: /delete
+net use U: /delete
+net use L: /delete
 net use X: /delete
+net use Y: /delete
