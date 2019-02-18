827e65e3410566fc35e080ede395e84545feb59c
matteo <matteo.lacki@gmail.com>
2019-02-18 16:11:00 +0100

    Eliminated os module. Added option -r to get_params.

--- projectizer2/projectizer2/get_params.py
+++ projectizer2/projectizer2/get_params.py
@@ -1,17 +1,22 @@
 import argparse
 
-from source import dump_params_to_jsons
+from source import dump_params_to_jsons, iter_output_path
 
 
 p = argparse.ArgumentParser(description="Dump parameters from xml files into jsons.")
 p.add_argument("folder", help="Folder to be searched for results.")
+p.add_argument( "-r", "--recursive",
+                action='store_const',
+                const=True,
+                default=False,
+                help="Show all verbosely.")
 p.add_argument( "-v", "--verbose",
                 action='store_const',
                 const=True,
                 default=False,
                 help="Show all verbosely.")
 a = p.parse_args()
-
 # fp = "/home/matteo/Projects/lab_scripts/projectizer2/data"
-# dump_params_to_jsons(fp, True)
-dump_params_to_jsons(a.folder, a.verbose)
+
+file_paths = iter_output_path(a.folder) if a.recursive else [a.folder]
+dump_params_to_jsons(file_paths, a.verbose)
